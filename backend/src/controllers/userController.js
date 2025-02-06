26f929cd4aaa7ece67da030967db9e94bde55315
Gopal Gupta <105534501+gopalgupta0007@users.noreply.github.com>
2025-02-06 14:21:15 +0530

    updated jwt token issues

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -35,22 +35,39 @@ const login = async (req, res) => {
         userFound = await Users.findOne({ email });
         if (userFound) {
             is_password_correct = await bcrypt.compare(password, userFound.password)
+
+            if (!is_password_correct) {
+                res.status(400).json({ error: "Invalid Credientials." })
+            } else {
+                // jwt
+                token = await userFound.generateAuthToken();
+                console.log("token =>", token);
+                res.cookie("jwt", token, {
+                    expires: new Date(Date.now() + 25892000000), // which is 25892000000=30days(30day * 24hour * 60mint * 60sec * 1000millisec)
+                    httpOnly: true
+                })
+
+                res.status(200).send({ massage: "login successfull", userFound });
+            }
+
+
         } else {
             res.status(500).json({ massage: "data not found" })
         };
-        if (!is_password_correct) { res.status(500).json({ massage: "data not found" }) };
+        // if (!is_password_correct) { res.status(500).json({ massage: "data not found" }) };
 
         //jwt => jsonwebtoken
-        token = await userFound.generateAuthToken();
-        console.log("token =>", token);
-        res.cookie("jwt", token, {
-            expires: new Date(Date.now() + 25892000000), // which is 25892000000=30days(30day * 24hour * 60mint * 60sec * 1000millisec)
-            httpOnly: true
-        })
+        // token = await userFound.generateAuthToken();
+        // console.log("token =>", token);
+        // res.cookie("jwt", token, {
+        //     expires: new Date(Date.now() + 25892000000), // which is 25892000000=30days(30day * 24hour * 60mint * 60sec * 1000millisec)
+        //     httpOnly: true
+        // })
+
     } catch (err) {
         return res.status(500).send(err);
     }
-    return res.status(200).send({ massage: "login successfull", userFound });
+    // return res.status(200).send({ massage: "login successfull", userFound });
 }
 
 
